9fdae8b23fc8724b2100b288ddd3fc2c27b3fea7
khalil <khalillakhdharatc@gmail.com>
2021-11-14 09:53:51 +0100

    Update GrhcodeApplicationTests.java

--- src/test/java/com/grh/GrhcodeApplicationTests.java
+++ src/test/java/com/grh/GrhcodeApplicationTests.java
@@ -1,6 +1,7 @@
 package com.grh;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -23,5 +24,14 @@ assertEquals(ch, "bonjour");
 void comparetoZero()
 {
 assertTrue(5>0);	
+}
+@Test
+public void whenExceptionThrown_thenAssertionSucceeds() {
+Exception exception = assertThrows(Exception.class, () -> {
+Integer.parseInt("1a");
+});
+String expectedMessage = "For input string";
+String actualMessage = exception.getMessage();
+assertTrue(actualMessage.contains(expectedMessage));
 }
 }
